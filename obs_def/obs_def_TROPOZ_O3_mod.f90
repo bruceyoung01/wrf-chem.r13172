96dcc41d6c8e27973d853bd9786b8cb697078ca4
vy57456 <vy57456@taki-usr1.cm.cluster>
2019-07-10 17:12:16 -0400

    Add file obs_def_TROPOZ_O3_mod.f90 in the directory obs_def, need to edit more

---
+++ obs_def/obs_def_TROPOZ_O3_mod.f90
@@ -0,0 +1,4 @@
+! $ID: obs_def_TROPOZ_O3_mod.f90 V01 07/10/2019 17:09 ZHIFENG YANG EXP$
+!
+!**********************************************************************
+!  PROGRAM obs_def_TROPOZ_O3_mod.f90 READS 
